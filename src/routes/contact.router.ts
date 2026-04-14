8f3a783b5579545c4b4ce0a3e4565a365a1e1bd7
Ignacio <185011694+nimbusphagia@users.noreply.github.com>
2026-04-13 21:03:18 -0500

    add contact routes

--- src/routes/contact.router.ts
+++ src/routes/contact.router.ts
@@ -2,4 +2,11 @@ import { Router } from "express";
 
 const contactRouter = Router();
 
+contactRouter.get("/");
+contactRouter.get("/:id");
+contactRouter.post("/");
+contactRouter.patch("/:id");
+contactRouter.delete("/:id");
+contactRouter.patch("/block/:id");
+
 export default contactRouter;
